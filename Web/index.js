dd0cb84ac9b5fb539b5ac25caff4f8c1fc39e227
Afonso ComboVideos <afonso.bezerra@combovideos.com.br>
2021-11-28 17:03:16 -0300

    Acertando Imagens e Nomes

--- Web/index.js
+++ Web/index.js
@@ -6,17 +6,3 @@ botaomenu.addEventListener('click', () =>{
     menu.classList.toggle('menu-lateral--ativo')
 })
 
-const mudarcor = document.querySelector('.switch')
-
-const body = document.querySelector('.body')
-const titulo_pagina = document.querySelector('.titulo-pagina')
-const titulo_secao = document.querySelector('.titulo-secao')
-const titulo_secao2 = document.querySelector('.titulo-secao2')
-
-mudarcor.addEventListener('click', () =>{
-    console.log("teste")
-    titulo_pagina.classList.toggle('titulo-claro')
-    titulo_secao.classList.toggle('titulo-claro')
-    titulo_secao2.classList.toggle('titulo-claro')
-    body.classList.toggle('body-claro')
-})
